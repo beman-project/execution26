4fd7bb73e2296ddd9ee1d17c26494593d47c682e
Dietmar Kühl <dietmar.kuehl@me.com>
2024-05-19 14:29:40 +0100

    added inplace_stop_token and family

---
+++ test/stopsource-inplace-cons.pass.cpp
@@ -0,0 +1,17 @@
+// Copyright © 2024 Beman Project
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <beman/stop_token.hpp>
+#include "test/execution.hpp"
+#include <cassert>
+
+auto main() -> int
+{
+    // Plan:
+    // - Given a default constructed inplace_stop_source.
+    // - Then stop_requested() yields true.
+    // Reference: [stopsource.inplace.general] p2
+
+    ::test_std::inplace_stop_source source;
+    assert(source.stop_requested() == false);
+}
