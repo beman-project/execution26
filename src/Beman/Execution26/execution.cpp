c81eaeb032711d528e5ef2ed80a538b01e65c5a1
Dietmar Kühl <dietmar.kuehl@me.com>
2024-07-14 19:15:39 +0100

    trying to fix the include path for the tests

---
+++ src/Beman/Execution26/execution.cpp
@@ -0,0 +1,8 @@
+// src/Beman/Execution26/execution.cpp -*-C++-*-
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// ----------------------------------------------------------------------------
+
+namespace Beman::Execution26
+{
+    int version{000};
+}
