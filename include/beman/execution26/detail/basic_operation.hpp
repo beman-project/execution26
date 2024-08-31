704bf468db9939a5dd7e5552cdbb986a72b0e65b
Dietmar Kühl <dietmar.kuehl@me.com>
2024-09-01 00:47:26 +0100

    used the generic facilities for basic_operation

--- include/beman/execution26/detail/basic_operation.hpp
+++ include/beman/execution26/detail/basic_operation.hpp
@@ -11,6 +11,7 @@
 #include <beman/execution26/detail/impls_for.hpp>
 #include <beman/execution26/detail/connect_all_result.hpp>
 #include <beman/execution26/detail/valid_specialization.hpp>
+#include <functional>
 #include <utility>
 
 // ----------------------------------------------------------------------------
@@ -44,34 +45,16 @@ namespace beman::execution26::detail
 
         auto start() & noexcept -> void
         {
-            if constexpr (requires(){
-                []{ auto&&[op0, op1, op2, op3] = *static_cast<inner_ops_t*>(nullptr); };
-                })
-            {
-                auto&[op0, op1, op2, op3] = this->inner_ops;
-                ::beman::execution26::detail::impls_for<tag_t>::start(this->state, this->receiver, op0, op1, op2, op3);
-            }
-            else if constexpr (requires(){
-                []{ auto&&[op0, op1, op2] = *static_cast<inner_ops_t*>(nullptr); };
-                })
-            {
-                auto&[op0, op1, op2] = this->inner_ops;
-                ::beman::execution26::detail::impls_for<tag_t>::start(this->state, this->receiver, op0, op1, op2);
-            }
-            else if constexpr (requires(){
-                []{ auto&&[op0, op1] = *static_cast<inner_ops_t*>(nullptr); };
-                })
-            {
-                auto&[op0, op1] = this->inner_ops;
-                ::beman::execution26::detail::impls_for<tag_t>::start(this->state, this->receiver, op0, op1);
-            }
-            else if constexpr (requires(){
-                []{ auto&&[op0] = *static_cast<inner_ops_t*>(nullptr); };
-                })
-            {
-                auto&[op0] = this->inner_ops;
-                ::beman::execution26::detail::impls_for<tag_t>::start(this->state, this->receiver, op0);
-            }
+            ::std::invoke([this]<::std::size_t... I>(::std::index_sequence<I...>)
+                {
+                    ::beman::execution26::detail::impls_for<tag_t>::start(
+                        this->state,
+                        this->receiver,
+                        this->inner_ops.template get<I>()...
+                        );
+                },
+                ::std::make_index_sequence<inner_ops_t::size()>{}
+            );
         }
     };
 }
