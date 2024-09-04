86559bf3c80375cdebe39529716a28164668f7ee
Dietmar Kühl <dietmar.kuehl@me.com>
2024-09-04 20:32:47 +0100

    made meta::unique stable

--- include/beman/execution26/detail/meta_unique.hpp
+++ include/beman/execution26/detail/meta_unique.hpp
@@ -13,7 +13,10 @@
 
 namespace beman::execution26::detail::meta::detail
 {
+    template <typename, typename> struct make_unique;
     template <typename> struct unique;
+
+#if 0
 
     template <template <typename...> class List>
     struct unique<List<>>
@@ -36,6 +39,35 @@ namespace beman::execution26::detail::meta::detail
             ::beman::execution26::detail::meta::prepend<H, tail>
             >;
     };
+#else
+    template <template <typename...> class List, typename... R>
+    struct make_unique<List<R...>, List<>>
+    {
+        using type = List<R...>;
+    };
+
+    template <template <typename...> class List, typename... R, typename H, typename... T>
+    struct make_unique<List<R...>, List<H, T...>>
+    {
+        using type = typename ::beman::execution26::detail::meta::detail::make_unique<
+            ::std::conditional_t<
+                ::beman::execution26::detail::meta::contains<H, R...>,
+                List<R...>,
+                List<R..., H>
+            >,
+            List<T...>
+        >::type;
+    };
+
+    template <template <typename...> class List, typename... T>
+    struct unique<List<T...>>
+    {
+        using type = typename ::beman::execution26::detail::meta::detail::make_unique<
+            List<>,
+            List<T...>
+        >::type;
+    };
+#endif
 }
 
 namespace beman::execution26::detail::meta
