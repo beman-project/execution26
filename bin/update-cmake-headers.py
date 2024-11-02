fbe7604c353344e9c547f987978fb533f0892568
ClausKlein <claus.klein@arcormail.de>
2024-11-02 08:22:58 +0100

    Format python files witch black

--- bin/update-cmake-headers.py
+++ bin/update-cmake-headers.py
@@ -8,40 +8,44 @@ import re
 import sys
 
 head_re = re.compile("include/(?P<name>.*)\.hpp")
+
+
 def clean_name(file):
     match = head_re.match(file)
     return match.group("name")
 
+
 def get_headers(dir):
     result = []
-    for header in glob.glob(dir + '/*.hpp'):
+    for header in glob.glob(dir + "/*.hpp"):
         result.append(clean_name(header))
     result.sort()
     return result
 
+
 sections = {
-    'public': get_headers('include/beman/*'),
-    'detail': get_headers('include/beman/*/detail'),
+    "public": get_headers("include/beman/*"),
+    "detail": get_headers("include/beman/*/detail"),
 }
 
 file_set_re = re.compile(" *FILE_SET.*")
-section_re  = re.compile(" *\${TARGET_LIBRARY}_(?P<section>.*)_headers$")
-header_re   = re.compile(" *\${PROJECT_SOURCE_DIR}/include/beman/.*/.*\.hpp")
+section_re = re.compile(" *\${TARGET_LIBRARY}_(?P<section>.*)_headers$")
+header_re = re.compile(" *\${PROJECT_SOURCE_DIR}/include/beman/.*/.*\.hpp")
 
 if len(sys.argv) != 2:
-    print(f'usage: {sys.argv[0]} <path>/CMakeLists.txt')
+    print(f"usage: {sys.argv[0]} <path>/CMakeLists.txt")
     sys.exit(1)
 
 cmake = sys.argv[1]
-print(f'updating {cmake}')
+print(f"updating {cmake}")
 
 section = ""
 section_done = False
 
-with open(cmake, 'r') as input:
+with open(cmake, "r") as input:
     lines = input.readlines()
 
-with open(f'{cmake}', 'w') as output:
+with open(f"{cmake}", "w") as output:
     for line in lines:
         if None != file_set_re.match(line):
             section = ""
@@ -54,7 +58,7 @@ with open(f'{cmake}', 'w') as output:
                 section_done = True
                 project = "${PROJECT_SOURCE_DIR}"
                 for header in sections[section]:
-                    output.write(f'         {project}/include/{header}.hpp\n')
+                    output.write(f"         {project}/include/{header}.hpp\n")
         else:
             output.write(line)
             pass
