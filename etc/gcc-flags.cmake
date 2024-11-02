7c45940b00893e2722e1480092a3fcc39df1b5f6
ClausKlein <claus.klein@arcormail.de>
2024-11-02 08:06:50 +0100

    CMake-format cmake modules too

--- etc/gcc-flags.cmake
+++ etc/gcc-flags.cmake
@@ -2,9 +2,7 @@ include_guard(GLOBAL)
 
 set(CMAKE_CXX_STANDARD 23)
 
-set(CMAKE_CXX_FLAGS
-   "-std=c++23 -Wall -Wextra "
-CACHE STRING "CXX_FLAGS" FORCE)
+set(CMAKE_CXX_FLAGS "-std=c++23 -Wall -Wextra " CACHE STRING "CXX_FLAGS" FORCE)
 
 set(CMAKE_CXX_FLAGS_DEBUG "-O0 -fno-inline -g3" CACHE STRING "C++ DEBUG Flags" FORCE)
 set(CMAKE_CXX_FLAGS_RELEASE "-Ofast -g0 -DNDEBUG" CACHE STRING "C++ Release Flags" FORCE)
--- etc/gcc-toolchain.cmake
+++ etc/gcc-toolchain.cmake
@@ -3,10 +3,9 @@ include_guard(GLOBAL)
 set(CMAKE_C_COMPILER gcc)
 set(CMAKE_CXX_COMPILER g++)
 
-set(CMAKE_CXX_FLAGS
-  "-std=c++20 \
-   -Wall -Wextra "
-CACHE STRING "CXX_FLAGS" FORCE)
+set(CMAKE_CXX_FLAGS "-std=c++20 \
+   -Wall -Wextra " CACHE STRING "CXX_FLAGS" FORCE
+)
 
 set(CMAKE_CXX_FLAGS_DEBUG "-O0 -fno-inline -g3" CACHE STRING "C++ DEBUG Flags" FORCE)
 set(CMAKE_CXX_FLAGS_RELEASE "-Ofast -g0 -DNDEBUG" CACHE STRING "C++ Release Flags" FORCE)
