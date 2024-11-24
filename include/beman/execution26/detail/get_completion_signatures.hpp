cb64cb7b074768447904aff5e5f1465a81d9e5d2
Maikel Nadolski <maikel.nadolski@gmail.com>
2024-11-24 06:02:13 +0100

    exec.coroutines: completion signatures

--- include/beman/execution26/detail/get_completion_signatures.hpp
+++ include/beman/execution26/detail/get_completion_signatures.hpp
@@ -4,9 +4,15 @@
 #ifndef INCLUDED_BEMAN_EXECUTION26_DETAIL_GET_COMPLETION_SIGNATURES
 #define INCLUDED_BEMAN_EXECUTION26_DETAIL_GET_COMPLETION_SIGNATURES
 
+#include <beman/execution26/detail/await_result_type.hpp>
+#include <beman/execution26/detail/completion_signatures.hpp>
+#include <beman/execution26/detail/env_promise.hpp>
+#include <beman/execution26/detail/get_domain_late.hpp> //-dk:TODO remove
+#include <beman/execution26/detail/is_awaitable.hpp>
 #include <beman/execution26/detail/transform_sender.hpp> //-dk:TODO remove
-#include <beman/execution26/detail/get_domain_late.hpp>  //-dk:TODO remove
+
 #include <concepts>
+#include <exception>
 #include <type_traits>
 #include <utility>
 
@@ -24,18 +30,27 @@ struct get_completion_signatures_t {
         }};
 
         using sender_type = ::std::remove_cvref_t<decltype(new_sender(sender, env))>;
+        using decayed_env = ::std::remove_cvref_t<Env>;
         if constexpr (requires { new_sender(sender, env).get_completion_signatures(env); })
             return decltype(new_sender(sender, env).get_completion_signatures(env)){};
         else if constexpr (requires { typename sender_type::completion_signatures; })
             return typename sender_type::completion_signatures{};
-        //-dk:TODO deal with awaitable
+        else if constexpr (::beman::execution26::detail::
+                               is_awaitable<sender_type, ::beman::execution26::detail::env_promise<decayed_env>>) {
+            return ::beman::execution26::completion_signatures<
+                ::beman::execution26::set_value_t(
+                    ::beman::execution26::detail::
+                        await_result_type<sender_type, ::beman::execution26::detail::env_promise<decayed_env>>),
+                ::beman::execution26::set_error_t(::std::exception_ptr),
+                ::beman::execution26::set_stopped_t()>{};
+        }
     }
 
   public:
     template <typename Sender, typename Env>
-        requires(not::std::same_as<void,
-                                   decltype(get_completion_signatures_t::get(::std::declval<Sender>(),
-                                                                             ::std::declval<Env>()))>)
+        requires(not ::std::same_as<void,
+                                    decltype(get_completion_signatures_t::get(::std::declval<Sender>(),
+                                                                              ::std::declval<Env>()))>)
     auto operator()(Sender&& sender, Env&& env) const noexcept {
         return this->get(::std::forward<Sender>(sender), ::std::forward<Env>(env));
     }
