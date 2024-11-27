6910adc7bd6c653ffbd0440ac78bc62e57221309
ClausKlein <claus.klein@arcormail.de>
2024-11-27 22:48:38 +0100

    Fix module example

--- examples/modules.cpp
+++ examples/modules.cpp
@@ -3,12 +3,18 @@
 
 // import std;
 #include <iostream>
+#include <string>
 #if __cpp_modules < 201907L
 #include <beman/execution26/execution.hpp>
 #else
-import beman.execution26;
+import beman_execution26;
 #endif
 
 namespace ex = beman::execution26;
 
-int main() {}
+int main() {
+    auto [result] = ex::sync_wait(ex::when_all(ex::just(std::string("hello, ")), ex::just(std::string("world"))) |
+                                  ex::then([](const auto& s1, const auto& s2) { return s1 + s2; }))
+                        .value_or(std::tuple(std::string("oops")));
+    std::cout << "result='" << result << "'\n";
+}
